7cf4ab79eb54c10e0aa218366bc787d0cdf9866d
Leaundrae Mckinney <pytechdrae@gmail.com>
2023-06-09 08:11:29 -0400

    Pass data to details page

--- home/src/components/DetailsPageParent/DetailsPageParent.tsx
+++ home/src/components/DetailsPageParent/DetailsPageParent.tsx
@@ -15,11 +15,35 @@ export const DetailsContainerParent = () => {
   const [country, setCountry] = React.useState<Country>();
 
   React.useEffect(() => {
-    if(context.currentCountry){
-      setCountry(context.currentCountry);
-      console.log(country);
-    }
+    setCountry(context.currentCountry);
   }, [context.currentCountry]);
+
+  const currency = React.useMemo(() => {
+    const key = Object.keys(context.currentCountry.currencies);
+    const currencies = key.map((value) => {
+      return context.currentCountry.currencies[value]?.name;
+    })
+    return currencies.join(", ");
+  }, [context.currentCountry]);
+
+  const borders = React.useMemo(() => {
+    const names: string[] = context.currentCountry.borders.map(
+      (value: string) => {
+        return context.countries.find(
+          (country: Country) =>
+            country.code.toUpperCase() === value.toUpperCase()
+        ).name.common;
+      }
+    );
+    console.log(names)
+    return names;
+  }, [context.currentCountry, context.countryNames]);
+
+  const languages = React.useMemo(() => {
+    const keys = Object.keys(context.currentCountry.languages);
+    const compiledLanguages = keys.map((value) => context.currentCountry.languages[value])
+    return compiledLanguages.join(', ');
+  }, []);
 
   return (
     <AppContainer
@@ -31,19 +55,27 @@ export const DetailsContainerParent = () => {
         <Link to="/">Back</Link>
         {context.currentCountry && (
           <>
-            <img src={context.currentCountry.flag.svg} alt={context.currentCountry.name.common} />
+            <img
+              src={context.currentCountry.flag.svg}
+              alt={context.currentCountry.name.common}
+            />
             <div>{context.currentCountry.name.common}</div>
             <div>
               <div>Population: {context.currentCountry.population}</div>
               <div>Region: {context.currentCountry.region}</div>
               <div>Sub Region: {context.currentCountry.subregion}</div>
               <div>Capital: {context.currentCountry.capital}</div>
-              <div>Top Level Domain: {context.currentCountry.topLevelDomain}</div>
-              <div>Currencies: {context.currentCountry.currencies[context.currentCountry.code.toUpperCase()]?.toString()}</div>
-              <div>Languages: {context.currentCountry.languages[0]}</div>
-              {context.currentCountry.borders.map((value:string,idx:number) => {
-                return <div key={idx}>{value}</div>
-              })}
+              <div>
+                Top Level Domain: {context.currentCountry.topLevelDomain}
+              </div>
+              <div>Currencies: {currency}</div>
+              <div>Languages: {languages}</div>
+              <div>
+                Border Countries:{" "}
+                {borders.map((value: string, idx: number) => {
+                  return <button key={idx}>{value}</button>;
+                })}
+              </div>
             </div>
           </>
         )}
