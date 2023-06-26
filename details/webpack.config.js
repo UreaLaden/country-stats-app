815a43b47501c06dca3a7aca05680a827a11ebb4
Leaundrae Mckinney <pytechdrae@gmail.com>
2023-06-26 08:51:27 -0400

    public path variable

--- details/webpack.config.js
+++ details/webpack.config.js
@@ -5,7 +5,7 @@ const Dotenv = require('dotenv-webpack');
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:9001/",
+    publicPath: `${process.env.PUBLIC_PATH}`,
   },
 
   resolve: {
